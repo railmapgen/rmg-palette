f416877346901739571ef7566384b2ba9f32ebb3
Chito Wong <chi.to.wong@outlook.com>
2022-01-20 02:23:58 +0800

    #4 Fix local development environment

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { cityList } from '@railmapgen/rmg-palette-resources/dist';
-import Guangzhou from '@railmapgen/rmg-palette-resources/dist/palettes/guangzhou.js';
+import Guangzhou from '@railmapgen/rmg-palette-resources/dist/palettes/guangzhou';
 
 export default function AppRoot() {
     return (
         <div>
             <h1>RMG Palette</h1>
 
-            {cityList.map(city => (
-                <div key={city.id}>{city.name.en}</div>
-            ))}
+            <ul>
+                {cityList.map(city => (
+                    <li key={city.id}>{city.name.en}</li>
+                ))}
+            </ul>
 
-            {Guangzhou.map(line => (
-                <div key={line.id}>{line.name.en}</div>
-            ))}
+            <ul>
+                {Guangzhou.map(line => (
+                    <div key={line.id}>{line.name.en}</div>
+                ))}
+            </ul>
         </div>
     );
 }
