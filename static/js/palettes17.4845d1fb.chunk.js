28e14d8738e6004eb7262067b934417ea2d7d2b8
RMG Release Agent <rmg.release.agent@users.noreply.github.com>
2022-02-27 07:16:03 +0000

    Release version 0.1.5 to PRD

---
+++ static/js/palettes17.4845d1fb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrmg_palette=self.webpackChunkrmg_palette||[]).push([[8189],{1902:function(e,n,r){r.r(n);n.default=[{id:"inner",name:{en:"Inner Circle"},colour:"#4f4c4d"},{id:"outer",name:{en:"Outer Circle"},colour:"#f57c14"}]}}]);
+//# sourceMappingURL=palettes17.4845d1fb.chunk.js.map
