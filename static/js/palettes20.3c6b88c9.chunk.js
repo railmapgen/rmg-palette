b627d7b9e1e713d09a81c7255695cbbd573db5e5
RMG Release Agent <rmg.release.agent@users.noreply.github.com>
2022-03-02 18:10:48 +0000

    Release version 0.1.6 to PRD

---
+++ static/js/palettes20.3c6b88c9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrmg_palette=self.webpackChunkrmg_palette||[]).push([[1847],{1902:function(e,n,r){r.r(n);n.default=[{id:"inner",name:{en:"Inner Circle"},colour:"#4f4c4d"},{id:"outer",name:{en:"Outer Circle"},colour:"#f57c14"}]}}]);
+//# sourceMappingURL=palettes20.3c6b88c9.chunk.js.map
--- static/js/palettes56.d3850e4a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrmg_palette=self.webpackChunkrmg_palette||[]).push([[3658],{2453:function(e,n,r){r.r(n);n.default=[{id:"yellow",name:{en:"Yellow Line"},colour:"#FABF00"},{id:"green",name:{en:"Green Line"},colour:"#0EAB4A"},{id:"bus",name:{en:"Bus"},colour:"#e9172a"},{id:"ferry",name:{en:"Ferry"},colour:"#82CEF3"}]}}]);
-//# sourceMappingURL=palettes56.d3850e4a.chunk.js.map
