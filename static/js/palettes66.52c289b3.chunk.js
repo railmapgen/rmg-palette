fe4f252a04b77ff228e907cdf12bf7c5aebefc93
RMG Release Agent <rmg.release.agent@users.noreply.github.com>
2022-03-20 04:21:44 +0000

    Release version 0.2.4 to PRD

---
+++ static/js/palettes66.52c289b3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrmg_palette=self.webpackChunkrmg_palette||[]).push([[2871],{2453:function(e,n,r){r.r(n);n.default=[{id:"yellow",name:{en:"Yellow Line"},colour:"#FABF00"},{id:"green",name:{en:"Green Line"},colour:"#0EAB4A"},{id:"bus",name:{en:"Bus"},colour:"#e9172a"},{id:"ferry",name:{en:"Ferry"},colour:"#82CEF3"}]}}]);
+//# sourceMappingURL=palettes66.52c289b3.chunk.js.map
