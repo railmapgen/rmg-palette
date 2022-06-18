92dacd729dfc93a9fb5ddd755b6b0f3f5674e1f7
Chito Wong <chi.to.wong@outlook.com>
2022-06-18 22:07:06 +0800

    #54 Fix quotation marks in issue details tag

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -15,12 +15,12 @@ export type Name = [string, string];
 
 export const GITHUB_ISSUE_PREAMBLE = '**Do not edit lines below, they are meant for bots only!!!**';
 export const getGitHubIssueCityBlock = (cityEntry: CityEntry | null) => {
-    return `<details repo='rmg-palette' type='city'>
+    return `<details repo="rmg-palette" type="city">
 ${JSON.stringify(cityEntry, null, 4)}
 </details>`;
 };
 export const getGitHubIssueLinesBlock = (lines: PaletteEntry[]) => {
-    return `<details repo='rmg-palette' type='lines'>
+    return `<details repo="rmg-palette" type="lines">
 ${JSON.stringify(lines, null, 4)}
 </details>`;
 };
