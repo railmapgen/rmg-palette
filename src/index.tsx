25ebcbb9a50fdbe84a1a4b76e1f71e467c4d85d1
Chito Wong <chi.to.wong@outlook.com>
2022-08-27 14:06:16 +0800

    #187 [DRY RUN] Add rmg-runtime support

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import rmgRuntime from '@railmapgen/rmg-runtime';
 import React from 'react';
 import { createRoot, Root } from 'react-dom/client';
 import App from './App';
@@ -10,3 +11,17 @@ const renderApp = () => {
 };
 
 renderApp();
+
+const dryRun = async () => {
+    try {
+        const appVersion = await rmgRuntime.getAppVersion();
+        const env = await rmgRuntime.getEnv();
+        const instance = await rmgRuntime.getInstance();
+        console.log(appVersion, env, instance);
+        console.log('ms since rmg-runtime loaded:', rmgRuntime.getMsSinceStartUp());
+    } catch (e) {
+        console.error('Unexpected error during dry run rmg-runtime', e);
+    }
+};
+
+dryRun().then();
