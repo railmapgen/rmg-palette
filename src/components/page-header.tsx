913b50fa077752efbc4d281ba43e9fa4fccd12a8
Chito Wong <chi.to.wong@outlook.com>
2022-12-09 19:26:04 +0800

    #287 Remove CRA eslint plugins

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { CountryCode, countryList } from '@railmapgen/rmg-palette-resources';
 import { useDispatch } from 'react-redux';
 import { setSelectedCountry } from '../redux/app/app-slice';
