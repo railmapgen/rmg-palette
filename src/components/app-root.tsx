b800b3446a3599749efeaf5bfefdb25ad4b829da
Chito Wong <chi.to.wong@outlook.com>
2023-03-03 01:22:04 +0800

    #444 Listen to colour mode change

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,7 +1,7 @@
 import { lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WindowHeader from './window-header';
-import { RmgErrorBoundary, RmgLoader, RmgWindow } from '@railmapgen/rmg-components';
+import { RmgErrorBoundary, RmgLoader, RmgThemeProvider, RmgWindow } from '@railmapgen/rmg-components';
 
 const PaletteView = lazy(() => import('./palette-view/palette-view'));
 const TicketView = lazy(() => import('./ticket-view/ticket-view'));
@@ -9,27 +9,29 @@ const TicketView = lazy(() => import('./ticket-view/ticket-view'));
 export default function AppRoot() {
     return (
         <BrowserRouter basename={import.meta.env.BASE_URL}>
-            <RmgWindow>
-                <WindowHeader />
-                <Routes>
-                    <Route
-                        path="/new"
-                        element={
-                            <RmgErrorBoundary suspenseFallback={<RmgLoader isIndeterminate />}>
-                                <TicketView />
-                            </RmgErrorBoundary>
-                        }
-                    />
-                    <Route
-                        path="/"
-                        element={
-                            <RmgErrorBoundary suspenseFallback={<RmgLoader isIndeterminate />}>
-                                <PaletteView />
-                            </RmgErrorBoundary>
-                        }
-                    />
-                </Routes>
-            </RmgWindow>
+            <RmgThemeProvider>
+                <RmgWindow>
+                    <WindowHeader />
+                    <Routes>
+                        <Route
+                            path="/new"
+                            element={
+                                <RmgErrorBoundary suspenseFallback={<RmgLoader isIndeterminate />}>
+                                    <TicketView />
+                                </RmgErrorBoundary>
+                            }
+                        />
+                        <Route
+                            path="/"
+                            element={
+                                <RmgErrorBoundary suspenseFallback={<RmgLoader isIndeterminate />}>
+                                    <PaletteView />
+                                </RmgErrorBoundary>
+                            }
+                        />
+                    </Routes>
+                </RmgWindow>
+            </RmgThemeProvider>
         </BrowserRouter>
     );
 }
