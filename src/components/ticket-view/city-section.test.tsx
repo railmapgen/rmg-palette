583bda7d95d41eabcb93486652ee869263e3e1a2
Chito Wong <chi.to.wong@outlook.com>
2023-08-24 19:28:34 +0800

    #779 Fix unit tests

--- src/components/ticket-view/city-section.test.tsx
+++ src/components/ticket-view/city-section.test.tsx
@@ -3,13 +3,52 @@ import CitySection from './city-section';
 import { fireEvent, screen, waitFor } from '@testing-library/react';
 import rootReducer from '../../redux';
 import { createMockRootStore } from '../../setupTests';
+import { vi } from 'vitest';
 
 const realStore = rootReducer.getState();
 const mockStore = createMockRootStore({ ...realStore, ticket: { ...realStore.ticket, country: 'CN' } });
 
+const originalFetch = global.fetch;
+const mockFetch = vi.fn();
+
 describe('CitySection', () => {
     afterEach(() => {
         mockStore.clearActions();
+        vi.clearAllMocks();
+        global.fetch = originalFetch;
+    });
+
+    beforeEach(() => {
+        mockFetch.mockImplementation(url => {
+            if (url.includes('guangzhou')) {
+                return Promise.resolve({
+                    json: () =>
+                        Promise.resolve([
+                            {
+                                id: 'gz1',
+                                name: {
+                                    en: 'Line 1',
+                                    zh: '1号线',
+                                },
+                                colour: '#F3D03E',
+                                fg: '#000',
+                            },
+                            {
+                                id: 'gz2',
+                                name: {
+                                    en: 'Line 2',
+                                    zh: '2号线',
+                                },
+                                colour: '#00629B',
+                            },
+                        ]),
+                });
+            } else {
+                console.warn('Unhandled fetch:', url);
+                return originalFetch(url);
+            }
+        });
+        global.fetch = mockFetch;
     });
 
     it('Can populate ticket when selecting an existing city', async () => {
