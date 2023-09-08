c99e6d204fc032f8b899ed98ba0fc3c9857bc1ab
Chito Wong <chi.to.wong@outlook.com>
2023-09-08 19:28:09 +0800

    #830 Fix cyclic import

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,13 +1,9 @@
 import cityConfigJson from './palettes/city-config.json';
 import countryConfigJson from './palettes/country-config.json';
-import { CityEntry, CountryEntry, PaletteEntry } from './types';
-import { cachedFetch } from './cached-fetch';
-
-export const getPalette = async (cityId: string, signal?: AbortSignal): Promise<PaletteEntry[]> => {
-    return await cachedFetch(`/rmg-palette/resources/palettes/${cityId}.json`, { signal });
-};
+import { CityEntry, CountryEntry } from './types';
 
 export * from './types';
+export * from './cached-fetch';
 export * from './updater';
 
 export const cityList = cityConfigJson as CityEntry[];
