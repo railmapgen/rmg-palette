3a4d74a25a96bcb0cb29bb22b5f2aab77c559b10
Chito Wong <chi.to.wong@outlook.com>
2025-02-13 00:05:26 +0800

    #1593 Revert Update submit-modal-step-submit.tsx

--- src/components/modal/submit-modal-step-submit.tsx
+++ src/components/modal/submit-modal-step-submit.tsx
@@ -50,7 +50,7 @@ export default function SubmitModalStepSubmit(props: SubmitModalStepSubmitProps)
     const manualSearchParams = new URLSearchParams({
         template: 'new-palettes-request.md',
         labels: 'resources',
-        title: cityEntry?.name?.en ?? '[CITY]',
+        title: 'Resources: New palettes of ' + cityEntry?.name?.en,
     });
 
     const handleCopy = async () => {
